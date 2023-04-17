41ee6c4461312edcdf914c96fa30faf65bd9e2b6
junsu-kwon <kjs940322@gmail.com>
2023-04-18 01:03:50 +0900

    230417 공부

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -27,6 +27,14 @@ export async function getAllPosts(): Promise<Post[]> {
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 }
 
-export async function getPostData(): Promise<PostData> {
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
+  const metadata = await getAllPosts().then((posts) =>
+    posts.find((post) => post.path === fileName),
+  );
+  if (!metadata)
+    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
 
+  const content = await readFile(filePath, 'utf-8');
+  return { ...metadata, content };
 }
