2f00bbb90a31caac3cb5e672178ed7353ac0b288
junsu-kwon <kjs940322@gmail.com>
2023-04-19 01:29:12 +0900

    230418 공부

--- src/components/AdjacentPostCard.tsx
+++ src/components/AdjacentPostCard.tsx
@@ -8,27 +8,32 @@ type Props = {
   type: 'prev' | 'next';
 };
 
-export default function AdjacentPostCard({ post, type }: Props) {
-  const { title, description, path } = post;
+const ICON_CLASS =
+  'text-5xl m-4 text-yellow-300 transition-all group-hover:text-6xl';
 
+export default function AdjacentPostCard({
+  post: { path, title, description },
+  type,
+}: Props) {
   return (
-    <Link href={`/posts/${path}`} className="relative w-full max-h-40">
+    <Link
+      href={`/posts/${path}`}
+      className="group relative w-full bg-black max-h-56"
+    >
       <Image
-        className="w-full opacity-60"
+        className="w-full opacity-40"
         src={`/images/posts/${path}.png`}
         alt={title}
-        width={250}
+        width={150}
         height={100}
       />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        {type === 'prev' && <FaArrowLeft className="absolute left-1/2" />}
-        
-        <div className="w-full">
-          <h2>{title}</h2>
-          <h3>{description}</h3>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex justify-around items-center text-white px-8">
+        {type === 'prev' && <FaArrowLeft className={ICON_CLASS} />}
+        <div className="w-full text-center">
+          <h3 className="text-3xl font-bold">{title}</h3>
+          <p className="font-bold">{description}</p>
         </div>
-
-        {type === 'next' && <FaArrowRight />}
+        {type === 'next' && <FaArrowRight className={ICON_CLASS} />}
       </div>
     </Link>
   );
