307413649acf51d01865f6ec677af58577ed60fc
junsu-kwon <kjs940322@gmail.com>
2023-04-21 22:23:19 +0900

    230421 공부

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,31 @@
+import Hero from '@/components/Hero';
+
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 export default function AboutPage() {
-  return <p>나에대해!</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 프론트엔드 개발자 <br />
+          유저친화적인 어플리케이션을 만드는 것이 저의 일입니다.
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          비드코칭연구소 (-Now) <br />
+          한밭대학교 (-2019) <br />
+          지족고등학교 (-2013)
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          Node.js, React, Next.js, PHP, MYSQL
+          <br />
+          SVN, Git
+          <br />
+          VS Code
+        </p>
+      </section>
+    </>
+  );
 }
